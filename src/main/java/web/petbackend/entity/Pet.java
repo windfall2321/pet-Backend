22a733ecef486dcb5272fcd5265dd8f45c8a185a
sxm <59765936+windfall2321@users.noreply.github.com>
2025-04-30 13:24:45 +0800

    sql数据库更新

---
+++ src/main/java/web/petbackend/entity/Pet.java
@@ -0,0 +1,12 @@
+package web.petbackend.entity;
+
+import lombok.Data;
+
+@Data
+public class Pet {
+    private int petId;
+    private String Name;
+    private int ownerId;
+    private String Breed;
+    private String Gender;
+}
