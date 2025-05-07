7fcbd243471d33a61d454a62b1a8f9fbcc3c0e29
sxm <59765936+windfall2321@users.noreply.github.com>
2025-05-07 21:44:03 +0800

    修改个人信息接口

--- src/main/java/web/petbackend/config/OpenApiConfig.java
+++ src/main/java/web/petbackend/config/OpenApiConfig.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
+    @Primary
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components()
