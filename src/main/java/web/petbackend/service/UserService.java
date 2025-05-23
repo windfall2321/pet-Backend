1b43d55d94045cbec2f0640c16d713ceb6f6045c
sxm <59765936+windfall2321@users.noreply.github.com>
2025-05-23 21:30:16 +0800

    用户头像改成本地图片路径

--- src/main/java/web/petbackend/service/UserService.java
+++ src/main/java/web/petbackend/service/UserService.java
@@ -1,5 +1,6 @@
 package web.petbackend.service;
 
+import org.springframework.web.multipart.MultipartFile;
 import web.petbackend.entity.User;
 
 public interface UserService {
@@ -8,4 +9,18 @@ public interface UserService {
     User getUserInfo(Integer userId);
     void logout(Integer userId);
     User updateUserInfo(Integer userId, User user);
+    
+    /**
+     * 上传用户头像
+     * @param userId 用户ID
+     * @param file 头像文件
+     * @return 更新后的用户信息
+     */
+    User uploadProfile(Integer userId, MultipartFile file);
+
+    /**
+     * 删除用户头像
+     * @param userId 用户ID
+     */
+    void deleteProfile(Integer userId);
 } 
