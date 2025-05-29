708793a470424bbd1460420a360307601104992a
tteokbokkikyu <152304405+tteokbokkikyu@users.noreply.github.com>
2025-05-29 19:59:47 +0800

    pet添加图片

--- src/main/java/web/petbackend/service/PetService.java
+++ src/main/java/web/petbackend/service/PetService.java
@@ -1,5 +1,6 @@
 package web.petbackend.service;
 
+import org.springframework.web.multipart.MultipartFile;
 import web.petbackend.entity.Pet;
 
 import java.util.List;
@@ -10,7 +11,9 @@ public interface PetService {
     void deletePet(Pet pet);
     List<Pet> getPets();
     Pet selectById(int petId);
+    Pet uploadImage(Integer petId, MultipartFile file);
 
+    void deleteImage(Integer petId);
     List<Pet> selectByOwnerId(int ownerId);
     void deleteByOwnerId(int ownerId);
 }
